3ea2d56ec61220a03e7c9d905f987657382d31ab
jesse <jsoldat@hotmail.com>
2017-05-05 07:14:15 +0700

    created header component

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router';
+
+class Header extends Component {
+	render() {
+		return (
+			<div className='row'>
+				<nav>
+					<div className='nav-wrapper'>
+						<div className='col s12'>
+							<a href="#" className='brand-logo'>UpStar Music</a>
+							<ul id="nav-mobile" className='right hide-on-med-and-down'>
+								<li>
+									<Link>Random Artist</Link>
+								</li>
+								<li>
+									<Link>Create Artisti</Link>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+			</div>
+		);
+	}
+};
+
+export default Header;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './Header';
 
 const Home = ({children}) => {
 	return (
 		<div className='container'>
-			Router
+			<Header />
 			{children}
 		</div>
 	);
